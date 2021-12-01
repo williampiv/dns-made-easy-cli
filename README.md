e4f20b6d6b3b22b822b72f0efc6fc1d8fbefb6de
William Perdue <wlperdue@perdueweb.com>
2021-11-30 23:58:15 -0800

    Add README and ref to crypto spec

---
+++ README.md
@@ -0,0 +1,3 @@
+# DNS Made Easy CLI
+
+A cli for use against the DNS Made Easy api [https://api-docs.dnsmadeeasy.com/](https://api-docs.dnsmadeeasy.com)
--- crypto.go
+++ crypto.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-// implements DNS Made Easy's HMAC function via WEBADDRESS
+// implements DNS Made Easy's HMAC function via https://api-docs.dnsmadeeasy.com/#5b98221f-37e9-4845-a349-5e959241b4a5
 func generateHMAC(secretKey string) (string, string) {
 	timeLocation, _ := time.LoadLocation("GMT")
 	currentTime := time.Now().In(timeLocation).Format(time.RFC1123)
